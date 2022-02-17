8ce18a4afbcc5bb9cfcd0c124dd495de9dc929d7
sattyam <sattyamthakur02@gmail.com>
2022-02-17 17:20:00 +0530

    Updates done

--- app3.js
+++ app3.js
@@ -1,11 +1,12 @@
-let slidePosition=0;
-const Slides= document.getElementsByClassName('crousel_item')
+let slidePosition=0;  //Initialising slidePositions
+const Slides= document.getElementsByClassName('crousel_item') //Images Refrenes
 const totalSlides= Slides.length
 // Buttons refrences
-console.log("Testng")
+console.log("Testing Console")
 const PrevButton=document.getElementById('arrow_prev')
 const NextButton=document.getElementById('arrow_next')
 
+//Adding event Listener to both buttons 
 PrevButton.addEventListener('click',()=>{
     moveToPrevSlide();
 })
@@ -13,6 +14,7 @@ PrevButton.addEventListener('click',()=>{
 NextButton.addEventListener('click',()=>{
     moveToNextSlide();
 })
+//Function for adding and removing the CSS visible and hidden class
 function updateSlidePosition(){
     for(let slide of Slides){
         console.log(slide)
@@ -22,6 +24,7 @@ function updateSlidePosition(){
     Slides[slidePosition].classList.add('crousel_item_visible')
 }
 
+//Defining function passed to buttons. 
 function moveToPrevSlide(){
     if(slidePosition===0){
         slidePosition=totalSlides-1
